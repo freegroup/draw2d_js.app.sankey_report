8d8eb18c6ec33d557727dcb769dde16979c307f4
Andreas Herz <andreas.herz@sap.com>
2016-06-28 18:32:15 +0200

    ensure min of weight thihnes

--- src/assets/javascript/View.js
+++ src/assets/javascript/View.js
@@ -43,7 +43,7 @@ sankey.View = draw2d.Canvas.extend({
         weights.forEach(function(weight){
             var conn = _this.getLine(weight.conn);
             if(conn!==null){
-                conn.setWeight(weight.value);
+                conn.setWeight( Math.max(2,weight.value));
             }
         });
 
